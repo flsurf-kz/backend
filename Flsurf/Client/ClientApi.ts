9a1f019d4b5228e841895258377bb0e8d5e51787
pikoUsername <galymzhan.amantaj@gmail.com>
2025-05-25 12:29:32 +0500

    YES

--- Flsurf/Client/ClientApi.ts
+++ Flsurf/Client/ClientApi.ts
@@ -629,6 +629,11 @@ export interface IClient {
     /**
      * @return OK
      */
+    getSkill(skillId: string): Promise<SkillModel>;
+
+    /**
+     * @return OK
+     */
     blockUser(userId: string): Promise<boolean>;
 
     /**
@@ -5399,6 +5404,46 @@ export class Client implements IClient {
             });
         }
         return Promise.resolve<SkillModel[]>(null as any);
+    }
+
+    /**
+     * @return OK
+     */
+    getSkill(skillId: string): Promise<SkillModel> {
+        let url_ = this.baseUrl + "/api/skill/{skillId}";
+        if (skillId === undefined || skillId === null)
+            throw new Error("The parameter 'skillId' must be defined.");
+        url_ = url_.replace("{skillId}", encodeURIComponent("" + skillId));
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_: RequestInit = {
+            method: "GET",
+            headers: {
+                "Accept": "text/plain"
+            }
+        };
+
+        return this.http.fetch(url_, options_).then((_response: Response) => {
+            return this.processGetSkill(_response);
+        });
+    }
+
+    protected processGetSkill(response: Response): Promise<SkillModel> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+            result200 = SkillModel.fromJS(resultData200);
+            return result200;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<SkillModel>(null as any);
     }
 
     /**
@@ -14122,8 +14167,8 @@ export interface ISkillEntity {
 }
 
 export class SkillModel implements ISkillModel {
-    id?: string;
-    name?: string | undefined;
+    id!: string;
+    name!: string;
 
     constructor(data?: ISkillModel) {
         if (data) {
@@ -14157,8 +14202,8 @@ export class SkillModel implements ISkillModel {
 }
 
 export interface ISkillModel {
-    id?: string;
-    name?: string | undefined;
+    id: string;
+    name: string;
 }
 
 export class StartChatWithFreelancerCommand implements IStartChatWithFreelancerCommand {
